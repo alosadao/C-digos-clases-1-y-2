df359c5bd7f37a160d9613052dab18751f672f4a
Antonio Losada <alosadao@unal.edu.co>
2025-04-08 11:35:47 -0500

    guardar los códigos en un repositorio

---
+++ 2025-04-04/helloworld.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+
+int main(void) {
+    std::string name;
+    std::cout << "Hello, whats your name\n";
+    std::getline(std::cin, name); 
+     std::cout << "Hello, " + name + "\n" ;
+    
+    return 0;
+}
