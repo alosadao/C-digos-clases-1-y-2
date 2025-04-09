4c6569c4431fb74179099e60205d1c31c601088c
Antonio Losada <alosadao@unal.edu.co>
2025-04-09 08:35:10 -0500

    guardar código clase

--- 2025-04-09/adivinarnumero.cpp
+++ 2025-04-09/adivinarnumero.cpp
@@ -11,15 +11,21 @@ informar si lo adivina o nols
 int main(void) {
     const int NUM = 10;
     int guessed_number;
+    for(;;) {
     std::cout <<"Adivina numero entre 1 y 100:\n";
     std::cin >> guessed_number;
 
     if (guessed_number == NUM){
         std::cout <<"GANASTE!!!\n";
-    } else {
-        std::cout <<"NO ganaste intentalo d3e nuevo\n";
-    }    
+        break;
+    } else if (guessed_number > NUM){
+        std::cout <<"Te pasaste.\n";
+    }  else { 
+        std::cout <<"Te falta.\n";
+    }  
+ }
     
 
     return 0;
+
 }
